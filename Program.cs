3bfcba5543cacfce551a794de13f4a7085a813a3
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-04-25 08:23:23 +0300

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nameLength = int.Parse(Console.ReadLine());
+
+            string[] peopleNames = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Func<string[], int, string[]> getNames = (peopleNames, nameLength) => 
+            {
+                List<string> selected = new List<string>();
+
+                for (int i = 0; i < peopleNames.Length; i++)
+                {
+                    if (peopleNames[i].Length <= nameLength)
+                    {
+                        selected.Add(peopleNames[i]);
+                    }
+                }
+
+                return selected.ToArray();
+            };
+
+            string[] selectedPeople = getNames(peopleNames, nameLength);
+
+            Action<string[]> printNames = PrintNames;
+            printNames(selectedPeople);
+        }
+
+        private static void PrintNames(string[] names)
+        {
+            Console.WriteLine(string.Join(Environment.NewLine, names));
+        }        
+    }
+}
